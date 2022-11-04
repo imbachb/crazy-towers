334a755692cbe6e1e3a08f969dee7ba7a018127f
Samuel Dobmann <samuel.dobmann@m-f.ch>
2022-11-04 14:32:23 +0100

    Bootstrap landing page

--- source/crazy-towers/src/app/app-routing.module.ts
+++ source/crazy-towers/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
+import { RegistrationComponent } from './registration/registration.component';
+import { HighscoresComponent } from './highscores/highscores.component';
+import { GameComponent } from './game/game.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'game',
+    component: GameComponent,
+  },
+  {
+    path: 'highscores',
+    component: HighscoresComponent,
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- source/crazy-towers/src/app/app.module.ts
+++ source/crazy-towers/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { HighscoresComponent } from './highscores/highscores.component';
+import { RegistrationComponent } from './registration/registration.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    LandingPageComponent,
+    HighscoresComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

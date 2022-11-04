2c552179c0dcdc7183bd546a101934946355de20
Samuel Dobmann <samuel.dobmann@m-f.ch>
2022-11-04 16:04:32 +0100

    Add registration

--- source/crazy-towers/src/app/app.module.ts
+++ source/crazy-towers/src/app/app.module.ts
@@ -9,6 +9,11 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { MatButtonModule } from '@angular/material/button';
 import { HighscoresComponent } from './highscores/highscores.component';
 import { RegistrationComponent } from './registration/registration.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @NgModule({
   declarations: [
@@ -22,6 +27,11 @@ import { RegistrationComponent } from './registration/registration.component'
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- source/crazy-towers/src/app/registration/registration.component.ts
+++ source/crazy-towers/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  highschool = new FormControl('', [Validators.required]);
+  registerForNewsletter = new FormControl(false);
+
+  profileForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    highschool: this.highschool,
+    registerForNewsletter: this.registerForNewsletter,
+  });
+
+  public availableHighschools = [
+    'ETH Zürich',
+    'UZH'
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    // TODO: Registration on backend
+    console.warn(this.profileForm.value);
   }
 
 }
